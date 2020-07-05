da3cb713054a4a13d0ecec202bb4d96a64497821
theSpiderDev <gabryellcorrea.dev@gmail.com>
2020-07-05 19:36:05 -0300

    feat: Todo-ninja colors, text and display

--- todo-ninja/src/views/Home.vue
+++ todo-ninja/src/views/Home.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
+    <p class="red white--text">red white--text</p>
+    <p class="pink lighten-4 red--text text--darken-4">pink lighten-4 red--text text--darken-4</p>
+    <h1 class="display-4">display-4</h1>
+    <h2 class="display-3">display-3</h2>
+    <h3 class="display-2">display-2</h3>
+    <h4 class="display-1">display-1</h4>
+    <p class="headline">headline</p>
+    <p class="subheading">subheading</p>
+    <p class="subheading font-weight-bold">subheading font-weight-bold</p>
+    <p class="subheading text-uppercase">subheading text-uppercase</p>
+    <p class="subheading text-lowercase">subheading text-lowercase</p>
+    <p class="subheading text-capitalize">subheading text-capitalize</p>
+    <p class="caption">caption</p>
   </div>
 </template>
 
